25de992605c97fa4edef86b4f2c2c9f96b89252a
Satya Fariz <satyafariznur@gmail.com>
2022-11-03 17:10:43 +0700

    skip creating admin

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 php artisan migrate
 php artisan db:seed --class="Database\Seeders\Badaso\BadasoSeeder"
-php artisan badaso:admin admin@admin.com --create
 php artisan storage:link
 npm install
 chown -R 1000:1000 "/root/.npm"
